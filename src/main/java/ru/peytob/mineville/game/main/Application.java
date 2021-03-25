a24a270f8512ce1d44d3f2a5d5337db6829402d6
Roman <peytob23@gmail.com>
2021-03-25 21:22:34 +0300

    250321
    Added class Initializer.
    Added class Application.
    Added class Game.

---
+++ src/main/java/ru/peytob/mineville/game/main/Application.java
@@ -0,0 +1,31 @@
+package ru.peytob.mineville.game.main;
+
+import ru.peytob.mineville.system.Window;
+
+public class Application {
+    private static boolean isInitialized = false;
+    private static Window window;
+    private static Game game;
+
+    public static void initialize() {
+        if (!isInitialized) {
+            isInitialized = true;
+            window = new Window("Mineville", 800, 600);
+            game = new Game();
+        }
+    }
+
+    public static void run() {
+        while (!window.isClose()) {
+            window.pollEvents();
+            game.tick();
+            game.draw();
+            window.display();
+        }
+    }
+
+    public static void destroy() {
+        game.destroy();
+        window.destroy();
+    }
+}
---
+++ src/main/java/ru/peytob/mineville/game/main/Game.java
@@ -0,0 +1,19 @@
+package ru.peytob.mineville.game.main;
+
+public class Game {
+    public Game() {
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void destroy() {
+
+    }
+}
